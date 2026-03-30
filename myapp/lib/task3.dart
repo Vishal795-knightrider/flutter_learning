fa2ca3f5afd7467cc53fe1cbd5cc04d350399883
Vishal795-knightrider <vk3293801@gmail.com>
2026-03-31 00:07:58 +0530

    updated task3

--- myapp/lib/task3.dart
+++ myapp/lib/task3.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       home: Scaffold(
         appBar: AppBar(
           title: Text("Vishal Kashyap"),
