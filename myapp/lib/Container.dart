88310efdfd891a2fd90d6427a2e55d5ec58a26b2
Vishal795-knightrider <vk3293801@gmail.com>
2026-03-31 15:05:24 +0530

    added Container

---
+++ myapp/lib/Container.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Container Example"),
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              borderRadius: BorderRadius.circular(80),
+            ),
+            child: Center(
+              child: Text(
+                "Hey Vishal",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
