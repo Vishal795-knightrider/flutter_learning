93fdecc3b072cbc1e191d55139d72b5f92ec7792
Vishal795-knightrider <vk3293801@gmail.com>
2026-04-02 23:48:22 +0530

    added row and column in container

--- myapp/lib/Container.dart
+++ myapp/lib/Container.dart
@@ -10,26 +10,80 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Container Example"),
+          title: Text("Row Column + Shadow"),
         ),
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.greenAccent,
-              borderRadius: BorderRadius.circular(80),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // 🔹 Row (horizontal)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+
+                // Box 1
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                        offset: Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Box 2
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                        offset: Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            child: Center(
-              child: Text(
-                "Hey Vishal",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
+
+            SizedBox(height: 40),
+
+            // 🔹 Column (vertical)
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black38,
+                    blurRadius: 12,
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  "Center Box",
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
