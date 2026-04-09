cced3c852dda7a9cc3ed5d42768f62133c98ed03
Vishal795-knightrider <vk3293801@gmail.com>
2026-04-09 22:52:45 +0530

    updated screen (resolve error)

--- myapp/lib/screens/login_screen.dart
+++ myapp/lib/screens/login_screen.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      body: Center(
+        child: Text("Welcome Vishal 🎉"),
+      ),
+    );
+  }
+}
+
+
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
