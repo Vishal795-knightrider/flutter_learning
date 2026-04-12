c6110cae63d1c6ca7743d2a5a7006e76989a5c49
Vishal795-knightrider <vk3293801@gmail.com>
2026-04-12 11:29:19 +0530

    updated bottomNavigation

---
+++ myapp/lib/Navigator/BottomNavigation.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BottomNavExample(),
+    );
+  }
+}
+
+class BottomNavExample extends StatefulWidget {
+  const BottomNavExample({super.key});
+
+  @override
+  State<BottomNavExample> createState() => _BottomNavExampleState();
+}
+
+class _BottomNavExampleState extends State<BottomNavExample> {
+  int selectedIndex = 0;
+
+  // List of screens
+  final List<Widget> pages = const [
+    Center(child: Text("Home Screen")),
+    Center(child: Text("Search Screen")),
+    Center(child: Text("Profile Screen")),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Bottom Navigation Example")),
+      body: pages[selectedIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
