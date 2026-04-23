75b9c567f7f7cd25e0149a885fce8681f55487cc
Vishal795-knightrider <vk3293801@gmail.com>
2026-04-23 18:12:41 +0530

    updated main

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,20 +1,14 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
-import 'UI/profile_card.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'screens/splash_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/bmi_screen.dart';
+import 'screens/info_screen.dart';
+import 'screens/about_screen.dart';
+void main()=>runApp(const MyApp());
+class MyApp extends StatelessWidget{
+ const MyApp({super.key});
+ Widget build(BuildContext c)=>MaterialApp(debugShowCheckedModeBanner:false,routes:{
+ '/':(_)=>const SplashScreen(),'/home':(_)=>const HomeScreen(),'/bmi':(_)=>const BmiScreen(),'/info':(_)=>const InfoScreen(),'/about':(_)=>const AboutScreen(),
+ });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const ProfileCardScreen(),
-    );
-  }
-}
